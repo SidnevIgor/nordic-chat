ac55f1ca30c09fbef99e774aed9dca52e1494c61
Igor Sidnev <mr.igor.sidnev@gmail.com>
2021-09-23 00:24:11 +0300

    Heroku deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCDOrsUANIVWlh1QFYlPxQO1py5Y_xxW8I",
+    authDomain: "nordic-chat.firebaseapp.com",
+    databaseURL: "https://nordic-chat-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "nordic-chat",
+    storageBucket: "nordic-chat.appspot.com",
+    messagingSenderId: "101437492582",
+    appId: "1:101437492582:web:7399e44c8cda204ab80ca7"
+  }
 };
